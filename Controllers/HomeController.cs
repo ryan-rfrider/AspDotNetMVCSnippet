bbf7e6e657560c1db22e67eb1160bc88885c70c0
Ryan <ryan.rfrider@gmail.com>
2025-03-03 15:04:46 +0800

    select option snippet

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace AspDotNetMVCSnippet.Controllers
 {
+    public class MyViewModel
+    {
+        public string TxtA { get; set; }
+        public string TxtB { get; set; }
+
+        public string SelA { get; set; }
+
+        public string SelListAValue { get; set; }
+        public List<SelectListItem> SelListA { get; set; }
+    }
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            MyViewModel model = new MyViewModel();
+            model.TxtA = "hello";
+            model.TxtB = "world";
+            model.SelA = "G";
+
+            model.SelListA = new List<SelectListItem>() {
+                new SelectListItem() {
+                    Text="Chose",
+                    Value=""
+                } ,
+                new SelectListItem() {
+                    Text="Apple",
+                    Value="A"
+                } ,
+                new SelectListItem() {
+                    Text="Facebook",
+                    Value="F"
+                } ,
+                new SelectListItem() {
+                    Text="Google",
+                    Value="G",
+                    Selected=true
+                } ,
+            }; 
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(MyViewModel vm)
+        {
+
+            vm.SelListA = new List<SelectListItem>() {
+                new SelectListItem() {
+                    Text="Chose",
+                    Value=""
+                } ,
+                new SelectListItem() {
+                    Text="Apple",
+                    Value="A"
+                } ,
+                new SelectListItem() {
+                    Text="Facebook",
+                    Value="F"
+                } ,
+                new SelectListItem() {
+                    Text="Google",
+                    Value="G", 
+                } ,
+            };
+
+            return View(vm);
         }
 
         public ActionResult About()
